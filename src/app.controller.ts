9418b79bf87c401a8d11dfd701e32a8e783f2922
devgoncalo <goncalopintoneto@gmail.com>
2024-03-02 20:51:26 +0000

    feat: Route to create a New Notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
 @Controller('notifications')
 export class AppController {
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: "You've received a new friend request!",
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
